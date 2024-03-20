20485ef150141877dcbcc4a715413f6f0e84da4e
Garou1207 <kevinbarrutia@outlook.com>
2024-03-19 21:42:07 -0500

    subiendo Proyecto Final

---
+++ app/src/main/java/pe/edu/idat/proyectofinal/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package pe.edu.idat.proyectofinal.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
